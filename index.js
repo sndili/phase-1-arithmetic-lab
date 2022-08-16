1fc117708ab728103c8d1e01906fd3cdd8326d19
Sylvia <ndilisylvia@gmail.com>
2022-08-16 18:38:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()+1);
+
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+
+const max = Math.max(2, 20, 12);
+
+console.log(multiply, random, mod, max)
